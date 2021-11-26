b6d704c3fef81a412e870be0aec45461dcc8dc4f
KamalDGRT <kamaldgrt@gmail.com>
2021-11-26 22:15:01 +0530

    implement: rendering templates

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dictionary = {
+        'insert_content': 'Now, rendering from first_app/index.html!'
+    }
+    return render(
+        request,
+        'first_app/index.html',
+        context=my_dictionary
+    )
--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+TEMPLATE_DIR = BASE_DIR.joinpath('templates')
+print(TEMPLATE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
