d37e6bfe7ed2b63217f46e95bd684aae5b807840
KamalDGRT <kamaldgrt@gmail.com>
2021-11-28 19:20:33 +0530

    feat: learn & implement migrations for a user table in ProTwo

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppTwo.models import User
 
 # Create your views here.
-def test(request):
-    message = {
-        'help_me': 'Help me from views.py of ProTwo!'
-    }
-    return render(
-        request,
-        'ProTwo/index.html',
-        context=message
-    )
+def help(request):
+    helpdict = {'help_insert':'HELP PAGE'}
+    return render(request,'AppTwo/help.html',context=helpdict)
+
+def index(request):
+    return render(request,'AppTwo/index.html')
+
+def users(request):
+    user_list = User.objects.order_by('first_name')
+    user_dict = {"users":user_list}
+    return render(request,'AppTwo/users.html',context=user_dict)
